7940001a965cb755b923ae59734ce45b43ac2053
Sayed-Mohamed8114 <sayed.Mohamed8114@gmail.com>
2026-03-27 16:28:50 +0200

    adding the basic structure fot SPA task

---
+++ level 2/single page SPA/src/App.jsx
@@ -0,0 +1,20 @@
+import Contact from "./Components/Contact"
+import Features from "./Components/Featutres"
+import Footer from "./Components/Footer"
+import Home from "./Components/Home"
+import Navbar from "./Components/Navbar"
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    <Features/>
+    <Contact/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
